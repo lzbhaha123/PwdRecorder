6f59d803bf6f78569b10aa4b1cfcb0ed9998cd88
lzbhaha123 <845381951@qq.com>
2021-10-13 12:16:45 +1300

    v1

---
+++ password_recorder/lib/main.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:path/path.dart';
+import 'package:sqflite/sqflite.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Password Recorder',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Password Recorder'),
+        ),
+        body: Login(),
+      ),
+    );
+  }
+}
+
+class Login extends StatelessWidget {
+  const Login({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Text("11111");
+  }
+}
